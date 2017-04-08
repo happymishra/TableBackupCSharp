b6f8308cc67a51b418cdbfe1dd78af2a9dbdfd36
rupeshmi <rupesh.mishra@visiblealpha.local>
2017-04-09 01:03:04 +0530

    Initial commit for table back in C sharp

---
+++ BackupModelTable/BackupModelTable/Constants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BackupModelTable
+{
+    class Constants
+    {
+        public static string devConString = "{destination server connection strign}";
+        public static string prodConString = "{source server connection string}";
+        public static string tableName = "{tablename}_backup_" + String.Format("{0:yyyyMMdd}", DateTime.Now);
+        public static string createTableQuery = "CREATE TABLE "  + tableName + 
+            @"(
+                [Id]                INT IDENTITY (1, 1) NOT NULL,
+                [UserName]          VARCHAR (50) NOT NULL,
+                [FirstName]         VARCHAR (50) NULL,
+                [LastName]          VARCHAR (50) NULL,
+            );";
+    }
+}
---
+++ BackupModelTable/BackupModelTable/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BackupModelTable
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            DataTable tableData = new DataTable();
+            using (SqlConnection prodCon = new SqlConnection(Constants.prodConString))
+            {
+                prodCon.Open();
+                SqlDataAdapter query = new SqlDataAdapter("Select * From {tablename}", prodCon);
+                query.Fill(tableData);
+                createModelBackup(tableData);
+            }
+        }
+
+        public static void createModelBackup(DataTable modelMetadata)
+        {
+            
+            using (SqlConnection devCon = new SqlConnection(Constants.devConString))
+            {
+                devCon.Open();
+                string createTableQuery = Constants.createTableQuery;
+                SqlCommand cmd = new SqlCommand(createTableQuery, devCon);
+                cmd.ExecuteNonQuery();
+                using (SqlBulkCopy bulkCopy = new SqlBulkCopy(devCon))
+                {
+                    bulkCopy.DestinationTableName = Constants.tableName;
+                    try
+                    {
+                        // Write from the source to the destination.
+                        bulkCopy.WriteToServer(modelMetadata);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
+        }
+    }
+}
